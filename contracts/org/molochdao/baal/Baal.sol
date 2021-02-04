23de8906f21e9eae6665b0711cd1f0841e6b86e6
Bill Warren <w.s.warren12@gmail.com>
2021-02-03 20:59:35 -0500

    Update Baal
    - Split out different the processing of different proposal types to reduce gas of process function calls
    - Check if previous proposal has been processed to avoid any funny business 
    - Allow for potential early execution of proposals
    - Switch to max and min voting period lengths to allow for more flexibility
    - Restrict proposal submissions to only members or trusted contacts

--- contracts/org/molochdao/baal/Baal.sol
+++ contracts/org/molochdao/baal/Baal.sol
@@ -25,23 +25,28 @@ contract ReentrancyGuard { // call wrapper for reentrancy check - see https://gi
 
 contract Baal is ReentrancyGuard {
     address[] public memberList; // array of member accounts summoned or added by proposal
+    address[] public contactList; // array of contacts
     uint256 public proposalCount; // counter for proposals submitted 
     uint256 public totalSupply; // counter for member votes minted - erc20 compatible
-    uint256 public votingPeriod; // period for members to cast votes on proposals in epoch time
+    uint256 public minVotingPeriod; // min period proposal voting in epoch time
+    uint256 public maxVotingPeriod; // max period for proposal voting in epoch time
     uint8 constant public decimals = 18; // decimals for erc20 vote accounting - 18 is default for ETH
     string public name; // name for erc20 vote accounting
     string public symbol; // symbol for erc20 vote accounting
     
     mapping(address => uint256) public balanceOf; // mapping member accounts to votes
     mapping(address => bool) public contractList; // mapping contract approved for member calls 
+    mapping(address => bool) public contacts; // mapping of approved addresses for proposals
     mapping(address => mapping(uint256 => bool)) public voted; // mapping proposal number to whether member voted 
     mapping(uint256 => Proposal) public proposals; // mapping proposal number to struct details
     
-    event SubmitProposal(address indexed proposer, address indexed target, uint256 proposal, uint256 value, bytes data, bool membership, string details); // emits when member submits proposal 
+    event SubmitProposal(address indexed proposer, address indexed target, uint256 proposal, uint256 value, bytes data, uint8 membership, string details); // emits when member submits proposal 
     event SubmitVote(address indexed member, uint256 proposal, bool approve); // emits when member submits vote on proposal
     event ProcessProposal(uint256 proposal); // emits when proposal is processed and finalized
     event Receive(address indexed sender, uint256 value); // emits when ether (ETH) is received
     event Transfer(address indexed from, address indexed to, uint256 amount); // emits when member votes are minted or burned
+    event ProcessMemberProposal(address indexed from, address indexed to, uint256 amount, uint256 proposal);
+    event ProcessMemberKick(address indexed from, address indexed to, uint256 amount, uint256 proposal);
     
     struct Proposal {
         address target; // account that receives low-level call `data` and ETH `value` - if `membership` is `true` and data `length` is 0, the account that will receive `value` votes - otherwise, the account that will lose votes
@@ -50,7 +55,7 @@ contract Baal is ReentrancyGuard {
         uint256 yesVotes; // counter for member yes votes to calculate approval on processing
         uint256 votingEnds; // termination date for proposal in seconds since epoch - derived from votingPeriod
         bytes data; // raw data sent to `target` account for low-level call
-        bool membership; // flags whether proposal involves adding votes `value` to member (`target`) - if `false`, member votes will be removed
+        uint8 membership; // 0 - not membership, 1 - add shares, 2 - burn shares, 3 - add contact
         bool processed; // flags whether proposal has been processed and executed
         string details; // context for proposal - could be IPFS hash, plaintext, or JSON
     }
@@ -58,13 +63,15 @@ contract Baal is ReentrancyGuard {
     /// @dev deploy Baal and create initial array of member accounts with specific vote weights
     /// @param summoners Accounts to add as members
     /// @param votes Voting weight per member
-    /// @param _votingPeriod Voting period in seconds for members to cast votes on proposals
+    /// @param _minVotingPeriod Min Voting period in seconds for members to cast votes on proposals
+    /// @param _minVotingPeriod Max Voting period in seconds for members to cast votes on proposals
     /// @param _name Name for erc20 vote accounting
     /// @param _symbol Symbol for erc20 vote accounting
-    constructor(address[] memory summoners, uint256[] memory votes, uint256 _votingPeriod, string memory _name, string memory _symbol) {
+    constructor(address[] memory summoners, uint256[] memory votes, uint256 _minVotingPeriod, uint256 _maxVotingPeriod, string memory _name, string memory _symbol) {
         for (uint256 i = 0; i < summoners.length; i++) {
              totalSupply += votes[i]; // total votes incremented by summoning
-             votingPeriod = _votingPeriod; 
+             minVotingPeriod = _minVotingPeriod; 
+             maxVotingPeriod = _maxVotingPeriod; 
              name = _name;
              symbol = _symbol;
              balanceOf[summoners[i]] = votes[i]; // vote weights granted to member
@@ -79,11 +86,14 @@ contract Baal is ReentrancyGuard {
     /// @param data Raw data sent to `target` account for low-level call 
     /// @param membership Flags whether proposal involves adding member votes - if `false`, then stage transaction to remove member votes
     /// @param details Context for proposal - could be IPFS hash, plaintext, or JSON
-    function submitProposal(address target, uint256 value, bytes calldata data, bool membership, string calldata details) external nonReentrant returns (uint256 count) {
+    function submitProposal(address target, uint256 value, uint256 votingLength, bytes calldata data, uint8 membership, string calldata details) external nonReentrant returns (uint256 count) {
+        require(balanceOf[msg.sender] > 0 || contacts[msg.sender] == true, "Baal:: Must be a member or contact");
+        require(votingLength >= minVotingPeriod && votingLength <= maxVotingPeriod, "Baal:: Voting period too long or short");
+        
         proposalCount++;
         uint256 proposal = proposalCount;
         
-        proposals[proposal] = Proposal(target, value, 0, 0, block.timestamp + votingPeriod, data, membership, false, details); // push params into proposal struct - start timer
+        proposals[proposal] = Proposal(target, value, 0, 0, block.timestamp + votingLength, data, membership, false, details); // push params into proposal struct - start timer
         
         emit SubmitProposal(msg.sender, target, proposal, value, data, membership, details);
         return proposal;
@@ -110,39 +120,82 @@ contract Baal is ReentrancyGuard {
     /// @dev Process proposal and execute low-level call or membership management - proposal must exist, be unprocessed, and voting period must be finished
     /// @param proposal Number of proposal in `proposals` mapping to process for execution
     function processProposal(uint256 proposal) external nonReentrant returns (bool success, bytes memory retData) {
-        require(proposal <= proposalCount, "!exist");
-        require(proposals[proposal].votingEnds <= block.timestamp, "!finished");
-        require(!proposals[proposal].processed, "processed");
+        require(processingReady(proposal), "Baal:: !ready for processing");
+        require(proposals[proposal].membership == 0, "Baal:: membership proposal");
       
-        bool membership = proposals[proposal].membership;
-        
-        if (proposals[proposal].yesVotes > proposals[proposal].noVotes) { // check if proposal approved by members
-            if (membership) { // check into membership proposal
-                address target = proposals[proposal].target;
-                uint256 value = proposals[proposal].value;
-                
-                if(balanceOf[target] == 0) {memberList.push(target);} // update list of member accounts if new
-                
-                totalSupply += value; // add to total member votes
-                balanceOf[target] += value; // add to member votes
-                
-                emit Transfer(address(this), target, value); // event reflects mint of erc20 votes
-            } else if (!membership && proposals[proposal].data.length == 0) { // alternatively, check into removal proposal
-                address target = proposals[proposal].target;
-                uint256 balance = balanceOf[target];
-                
-                totalSupply -= balance; // subtract from total member votes
-                balanceOf[target] = 0; // reset member votes
-                
-                emit Transfer(address(this), address(0), balance); // event reflects burn of erc20 votes
-            } else { // otherwise, check into low-level call 
-                (bool callSuccess, bytes memory returnData) = proposals[proposal].target.call{value: proposals[proposal].value}(proposals[proposal].data); // execute low-level call
-                return (callSuccess, returnData); // return call success and data
-            }
+        bool _didPass = didPass(proposal);
+        if (_didPass){
+            (bool callSuccess, bytes memory returnData) = proposals[proposal].target.call{value: proposals[proposal].value}(proposals[proposal].data); // execute low-level call
+            require(callSuccess, "Baal:: action failed");
+            return (callSuccess, returnData); // return call success and data
         }
         
         proposals[proposal].processed = true; // reflect proposal processed
         emit ProcessProposal(proposal);
+    }
+    
+    
+    /// @dev Process proposal for membership management - proposal must exist, be unprocessed, and voting period must be finished
+    /// @param proposal Number of proposal in `proposals` mapping to process for execution
+    function processMemberProposal(uint256 proposal) external nonReentrant returns (uint256 shares) {
+        require(processingReady(proposal), "Baal:: !ready for processing");
+        require(proposals[proposal].membership == 1, "Baal:: !membership proposal");
+        
+        bool _didPass = didPass(proposal);
+        if (_didPass) { // check if proposal approved by members
+            address target = proposals[proposal].target;
+            uint256 value = proposals[proposal].value;
+                
+            if(balanceOf[target] == 0) {memberList.push(target);} // update list of member accounts if new
+                
+            totalSupply += value; // add to total member votes
+            balanceOf[target] += value; // add to member votes
+            
+            return(value);
+        }
+        
+        proposals[proposal].processed = true; // reflect proposal processed
+        emit ProcessMemberProposal(address(this), proposals[proposal].target, proposals[proposal].value, proposal); // event reflects mint of erc20 votes
+    }
+    
+    function processMemberKick(uint256 proposal) external nonReentrant returns (bool success) {
+        require(processingReady(proposal), "Baal:: !ready for processing");
+        require(proposals[proposal].membership == 2 && proposals[proposal].data.length == 0, "Baal:: !membership kick proposal");
+
+        bool _didPass = didPass(proposal);
+
+        if (_didPass) {
+            address target = proposals[proposal].target;
+            uint256 balance = balanceOf[target];
+                
+            totalSupply -= balance; // subtract from total member votes
+            balanceOf[target] = 0; // reset member votes
+                
+            return(true);
+        }
+        
+        proposals[proposal].processed = true; // reflect proposal processed
+        emit ProcessMemberKick(address(this), proposals[proposal].target, proposals[proposal].value, proposal); // event reflects mint of erc20 votes
+    }
+    
+    function processNewContact(uint256 proposal) external nonReentrant returns (bool success) {
+        require(processingReady(proposal), "Baal:: !ready for processing");
+        require(proposals[proposal].membership == 3 && proposals[proposal].data.length == 0, "Baal:: !contact");
+
+        bool _didPass = didPass(proposal);
+
+        if (_didPass) {
+            
+            address target = proposals[proposal].target;
+            contacts[target] = true;
+            contactList.push(target);
+            
+            return(true);
+        }
+        
+        proposals[proposal].processed = true; // reflect proposal processed
+        
+        emit ProcessMemberKick(address(this), proposals[proposal].target, proposals[proposal].value, proposal); // event reflects mint of erc20 votes
     }
     
     /// @dev Execute member action against external contract - caller must have votes
@@ -165,9 +218,37 @@ contract Baal is ReentrancyGuard {
         }
     }
     
+     /// @dev Checks if proposal passed
+    function didPass(uint256 proposal) internal view returns (bool) {
+        require(proposals[proposal].yesVotes > proposals[proposal].noVotes, "Baal::proposal failed");
+        return true;
+    }
+    
+    /// @dev Checks if proposal is ready to be processed (allows for possible early execution)
+    function processingReady(uint256 proposal) internal view returns (bool) {
+        require(proposal <= proposalCount, "Baal::!exist");
+        require(!proposals[proposal].processed, "Baal:: already processed");
+        require(proposalCount == 1 || proposals[proposalCount-1].processed, "previous proposal must be processed");
+        
+        uint256 halfShares = totalSupply / 2;
+        
+        if(proposals[proposal].votingEnds >= block.timestamp){ // voting period done
+            return true;
+        } else if(proposals[proposal].yesVotes > halfShares ) { // early execution b/c of 50%+
+            return true;
+        } else {
+            return false; //not ready
+        }
+    }
+    
     /// @dev Return array list of member accounts in Baal
     function getMembers() external view returns (address[] memory membership) {
         return memberList;
+    }
+    
+        /// @dev Return array list of member accounts in Baal
+    function getContacts() external view returns (address[] memory allContacts) {
+        return contactList;
     }
     
     /// @dev fallback to collect received ether into Baal
